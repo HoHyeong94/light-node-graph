99e7e6a50b6c087525bb08ab4b6206d430b02050
jayhans <dr.lim@nexivil.com>
2020-05-08 13:25:17 +0900

    sapframe modi

--- diSsolve/analysis/index.js
+++ diSsolve/analysis/index.js
@@ -30,6 +30,7 @@ export function SapFrame() {
     this.addInput("girderStation", "girderStation");
     this.addInput("sectionPointDict", "sectionPointDict");
     this.addInput("xbeamData", "xbeamData");
+    this.addInput("supportData", "supportData");
     this.addInput("nodeNumDict", "nodeNumDict");
     this.addInput("materials", "arr");
     this.addInput("sectionDB", "sectionDB");
@@ -38,7 +39,7 @@ export function SapFrame() {
 }
 
 SapFrame.prototype.onExecute = function () {
-    const result = SapFrameGenerator(this.getInputData(0), this.getInputData(1), this.getInputData(2),this.getInputData(3),this.getInputData(4),this.getInputData(5))
+    const result = SapFrameGenerator(this.getInputData(0), this.getInputData(1), this.getInputData(2),this.getInputData(3),this.getInputData(4),this.getInputData(5),this.getInputData(6))
     this.setOutputData(0, result.sectionPropDict)
     this.setOutputData(1, result.input)
 }
