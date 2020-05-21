87943bbcc5fa3301fa18baadab30be3abf37fbd5
jayhans <dr.lim@nexivil.com>
2020-05-21 17:52:23 +0900

    m

--- diSsolve/girder/module.js
+++ diSsolve/girder/module.js
@@ -306,7 +306,7 @@ export function splineProp(point1, point2) {
     + Math.sqrt(Math.pow((2 * coe.a1 * t2 + coe.b1), 2) + Math.pow((2 * coe.a2 * t2 + coe.b2), 2)) * w2
     + Math.sqrt(Math.pow((2 * coe.a1 * t3 + coe.b1), 2) + Math.pow((2 * coe.a2 * t3 + coe.b2), 2)) * w3;
   let l = Math.sqrt(coe.b2 ** 2 + coe.b1 ** 2)
-  let midPoint = { x: coe.c2, y: coe.c1, cos: coe.b2 / l, sin: coe.b1 / l, normalCos: coe.b1 / l, normalSin: -coe.b2 / l }
+  let midPoint = { x: coe.c2, y: coe.c1, cos: coe.b2 / l, sin: coe.b1 / l, normalCos: coe.b1 / l, normalSin: -coe.b2 / l, coe : coe }
   return { length: length.toFixed(4) * 1, midPoint }
 }
 
