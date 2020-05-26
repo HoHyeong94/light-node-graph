c5bcb0da2e84d096d4d3c7e8c17890730ac0d3ef
jayhans <dr.lim@nexivil.com>
2020-05-26 18:49:35 +0900

    re

--- diSsolve/line/module.js
+++ diSsolve/line/module.js
@@ -546,8 +546,8 @@ export const OffsetPoint = (masterPoint,masterLine,offset) => {
       resultPoint.z = masterPoint.z + resultPoint.gradientY * offset;
     } else {
       let skewRad = (masterPoint.skew-90)*Math.PI/180;
-      let cos = -Math.cos(skewRad); //반시계방향으로 회전
-      let sin = -Math.sin(skewRad);
+      let cos = Math.cos(skewRad); //반시계방향으로 회전
+      let sin = Math.sin(skewRad);
       let skewCos = masterPoint.normalCos*cos - masterPoint.normalSin*sin
       let skewSin = masterPoint.normalCos*sin + masterPoint.normalSin*cos
       let skewC = masterPoint.x * skewSin - masterPoint.y * skewCos
@@ -563,7 +563,7 @@ export const OffsetPoint = (masterPoint,masterLine,offset) => {
         let newCos = newP.normalCos
         let newSin = newP.normalSin
         let newC = newP.x * newSin - newP.y * newCos
-        let sign = offset>0?-1:1;
+        let sign = offset>0?1:-1;
         x = (skewCos*newC - newCos * skewC)/(skewCos *newSin - skewSin * newCos)
         y = (skewSin*newC - newSin * skewC)/(skewCos *newSin - skewSin * newCos)
         dist = Math.sqrt((newP.x - x)**2 + (newP.y - y)**2)
