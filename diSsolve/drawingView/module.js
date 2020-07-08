e1a458bc5ffb937c6eb9ab63cd3a5e9a84535bcd
jayhans <dr.lim@nexivil.com>
2020-07-08 16:32:21 +0900

    m

--- diSsolve/drawingView/module.js
+++ diSsolve/drawingView/module.js
@@ -1067,7 +1067,7 @@ export function PartGeneralDraw(diaDict, girderStation, layout) {
             let centerPoint = diaDict[i][key].point;
             let cos = Math.cos(diaDict[i][key].rotationY)
             let cosx = Math.cos(diaDict[i][key].rotationX)
-            console.log("check", centerPoint, centerPoint.normalCos)
+            console.log("check", centerPoint)
             // let rot = Math.atan2(centerPoint.normalCos, - centerPoint.normalSin) + rotate[index];
 
             if (diaDict[i][key].topView) {
