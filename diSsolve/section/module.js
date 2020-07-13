554723a238b9d1ce33c5821ae2b1786e133a216e
jayhans <dr.lim@nexivil.com>
2020-07-13 18:38:39 +0900

    m

--- diSsolve/section/module.js
+++ diSsolve/section/module.js
@@ -473,7 +473,7 @@ export function DeckSectionPoint(
             } else {
                 lw = UflangePoint(girderPoint, pointDict, girderBaseInfo[j], slabInfo, slabLayout);
             }
-
+            console.log(centerLineStations[i].key, lw)
             lw.forEach(elem => glw.push({ x: elem.x + girderPoint.offset, y: elem.y + girderPoint.z }))
             //haunch포인트에 대한 내용을 위의함수에 포함하여야 함. 
             //추후 three.js union함수를 통한 바닥판 계산을 하는것은 어떨지 고민중
