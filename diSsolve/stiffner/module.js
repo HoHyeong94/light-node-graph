fab2dcf21b1a85f0046b024ff4f68bd717b29bd5
jayhans <dr.lim@nexivil.com>
2020-08-21 14:54:58 +0900

    m

--- diSsolve/stiffner/module.js
+++ diSsolve/stiffner/module.js
@@ -1655,9 +1655,9 @@ export function hBracingSection(point1, point2, webPoints, hBSection, sectionDB)
   let [frame1, frame2] = Kframe({x:0,y: -VectorLength/2}, {x:0,y: VectorLength/2}, spc, spc, pts)
   let z1 =  pts[4]>0? 0: pts[4]
   let z2 =  pts[5]>0? 0: pts[5]
-  let rotX = Math.atan(Vector[2]/VectorLength2D)
-  result['frame1'] = hPlateGen(frame1, centerPoint, Math.abs(pts[4]),z1,90,0,0,null,true,null)
-  result['frame2'] = hPlateGen(frame2, centerPoint, Math.abs(pts[5]),z2,90,0,0,null,true,null)
+  let rotX = -Math.atan(Vector[2]/VectorLength2D)
+  result['frame1'] = hPlateGen(frame1, centerPoint, Math.abs(pts[4]),z1,90,rotX,0,null,true,null)
+  result['frame2'] = hPlateGen(frame2, centerPoint, Math.abs(pts[5]),z2,90,rotX,0,null,true,null)
   return result 
 }
 
