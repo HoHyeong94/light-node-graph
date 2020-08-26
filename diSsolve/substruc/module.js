1f2dbdadc2a66db4ab3c6ecdaf351428ea4f17fc
jayhans <dr.lim@nexivil.com>
2020-08-26 12:22:17 +0900

    m

--- diSsolve/substruc/module.js
+++ diSsolve/substruc/module.js
@@ -1,13 +1,13 @@
-import { ToGlobalPoint3} from "../geometryModule"
+import { ToGlobalPoint3, DividingPoint } from "../geometryModule"
 import { OffsetPoint } from "../line/module"
 
-export function AbutPointGen(girderLayout, masterLine, slabLayout){
+export function AbutPointGen(girderLayout, masterLine, slabLayout) {
     let masterPoint = girderLayout.startPoint
     let leftOffset = slabLayout[0][3]
     let rightOffset = slabLayout[0][4]
     let leftPoint = OffsetPoint(masterPoint, masterLine, leftOffset);
     let rightPoint = OffsetPoint(masterPoint, masterLine, rightOffset);
-    return  [leftPoint, masterPoint, rightPoint]
+    return [leftPoint, masterPoint, rightPoint]
 }
 export function AbutModelGen(abutPoints, abutInput, sectionPointDict, supportLayout) {
     let model = {}; // for loftModel
@@ -18,21 +18,28 @@ export function AbutModelGen(abutPoints, abutInput, sectionPointDict, supportLay
         backHaunchThick: 600,
         approachDepth: 300,
         approachHeight: 800,
-        supportDepth : 1400,
+        supportDepth: 1400,
         ELsub: 18000,
         footHeight: 1600,
         footLengthB: 3700,
         footLengthf: 1600,
         LeanConcT: 100,
-        LeanConcL: 100
+        LeanConcL: 100,
+        wingWallThick: 800,
+        wingLength : 6500,
+        wingH1 : 1500,
+        wingH2 : 2600,
+        wingL1 : 2600,
+        wingGradient : 0.02,
     };
     let abutDepth = 2500; // 거더높이 + 하부플렌지두께 + 솔플레이트두께 + 받침높이 + 페데스탈높이 중 최대깊이
     let abutHeight = 5000;
-    model["Start"] = {"points" : [], "ptGroup" : []}
-    for (let pt in abutPoints) {
+    let points = []
+    model["Start"] = { "points": [], "ptGroup": [] }
+    for (let index in abutPoints) {
         model["Start"]["points"].push([]);
-        let totalH = abutPoints[pt].z - tempInput.ELsub
-        let points = [{ x: 0, y: 0 },//시점을 기준으로 시계반대방향 순
+        let totalH = abutPoints[index].z - tempInput.ELsub
+        points = [{ x: 0, y: 0 },//시점을 기준으로 시계반대방향 순
         { x: -tempInput.backWallThick + tempInput.approachDepth, y: 0 },
         { x: -tempInput.backWallThick + tempInput.approachDepth, y: -tempInput.approachHeight },
         { x: -tempInput.backWallThick, y: -tempInput.approachHeight },
@@ -42,13 +49,41 @@ export function AbutModelGen(abutPoints, abutInput, sectionPointDict, supportLay
         { x: -tempInput.backWallThick + tempInput.backHaunchThick - tempInput.footLengthB, y: -totalH + tempInput.backHaunchHeight },
         { x: -tempInput.backWallThick + tempInput.backHaunchThick - tempInput.footLengthB, y: -totalH },
         { x: tempInput.supportDepth + tempInput.footLengthf, y: -totalH },
-        { x: tempInput.supportDepth + tempInput.footLengthf, y: -totalH +  tempInput.backHaunchHeight},
-        { x: tempInput.supportDepth, y: -totalH +  tempInput.backHaunchHeight},
-        { x: tempInput.supportDepth, y: -totalH +  tempInput.backHaunchHeight + abutHeight},
-        { x: 0, y: -totalH +  tempInput.backHaunchHeight + abutHeight},
+        { x: tempInput.supportDepth + tempInput.footLengthf, y: -totalH + tempInput.backHaunchHeight },
+        { x: tempInput.supportDepth, y: -totalH + tempInput.backHaunchHeight },
+        { x: tempInput.supportDepth, y: -totalH + tempInput.backHaunchHeight + abutHeight },
+        { x: 0, y: -totalH + tempInput.backHaunchHeight + abutHeight },
         ];
-        points.forEach(npt => model["Start"]["points"][pt].push(ToGlobalPoint3(abutPoints[pt], npt)))
+        points.forEach(npt => model["Start"]["points"][index].push(ToGlobalPoint3(abutPoints[index], npt)))
     }
-    model["Start"]["ptGroup"] = [[0,1,2,13], [2,3,4,5,13], [5,6,13],[6,11,12,13], [7,8,9,10]];
+    model["Start"]["ptGroup"] = [[0, 1, 2, 13], [2, 3, 4, 5, 13], [5, 6, 13], [6, 11, 12, 13], [7, 8, 9, 10]];
+    //우선 직각인 날개벽을 예시로함
+    let pt1 = {};
+    let pt2 = {};
+    let npt = [];
+    let wingPoints = [
+                    {x : points[0].x - tempInput.wingLength ,y: points[0].y - tempInput.wingLength * tempInput.wingGradient},
+                    {x : points[0].x - tempInput.wingLength ,y: points[0].y - tempInput.wingLength * tempInput.wingGradient - tempInput.wingH1},
+                    {x : points[7].x ,y: points[0].y - tempInput.wingLength * tempInput.wingGradient - tempInput.wingH1 - tempInput.wingH2},
+                    points[7], points[6], points[5], points[4], points[3], points[2], points[1]]
+
+    let wingPt1 = [];
+    wingPoints.push(pt => wingPt1.push(ToGlobalPoint3(abutPoints[0],pt)))
+    let cos = abutPoints[0].normalCos
+    let sin = abutPoints[0].normalSin
+    let dx = tempInput.wingWallThick * cos
+    let dy = tempInput.wingWallThick * sin
+    let wingPt2 = [{x: wingPt1[0].x + dx, y: wingPt1[0].y + dy, z: wingPt1[0].z},
+                    {x: wingPt1[1].x + dx, y: wingPt1[1].y + dy, z: wingPt1[1].z},
+                    {x: wingPt1[2].x + dx, y: wingPt1[2].y + dy, z: wingPt1[2].z},
+                    {x: wingPt1[3].x + dx, y: wingPt1[3].y + dy, z: wingPt1[3].z},];
+    for (let i of [6, 5, 4, 3, 2, 1]) {
+        pt1 = model["Start"]["points"][0][i];
+        pt2 = model["Start"]["points"][1][i];
+        let l = Math.sqrt((pt1.x - pt2.x)**2 + (pt1.y - pt2.y)**2 + (pt1.z - pt2.z)**2)
+        let l2D = Math.sqrt((pt1.x - pt2.x)**2 + (pt1.y - pt2.y)**2)
+        npt.push(DividingPoint(pt1, pt2, tempInput.wingWallThick * l / l2D));
+    }
+    model["leftWing"] = { "points": [wingPt1, wingPt2], "ptGroup": [[0,7,8,9],[0,1,2,6,7],[2,3,4,5,6]]  }
     return model
 }
