f4b04102f3d88aa4bc299a0bbeb7aea781c929f9
jayhans <dr.lim@nexivil.com>
2020-08-26 14:17:25 +0900

    m

--- diSsolve/substruc/module.js
+++ diSsolve/substruc/module.js
@@ -26,11 +26,12 @@ export function AbutModelGen(abutPoints, abutInput, sectionPointDict, supportLay
         LeanConcT: 100,
         LeanConcL: 100,
         wingWallThick: 800,
-        wingLength : 6500,
-        wingH1 : 1500,
-        wingH2 : 2600,
-        wingL1 : 2600,
-        wingGradient : 0.02,
+        wingLength: 6500,
+        wingH1: 1500,
+        wingH2: 2600,
+        wingL1: 2600,
+        wingGradient: 0.02,
+        wingHaunch: 300,
     };
     let abutDepth = 2500; // 거더높이 + 하부플렌지두께 + 솔플레이트두께 + 받침높이 + 페데스탈높이 중 최대깊이
     let abutHeight = 5000;
@@ -62,29 +63,44 @@ export function AbutModelGen(abutPoints, abutInput, sectionPointDict, supportLay
     let pt2 = {};
     // let npt = [];
     let wingPoints = [
-                    {x : points[0][0].x - tempInput.wingLength ,y: points[0][0].y - tempInput.wingLength * tempInput.wingGradient},
-                    {x : points[0][0].x - tempInput.wingLength ,y: points[0][0].y - tempInput.wingLength * tempInput.wingGradient - tempInput.wingH1},
-                    {x : points[0][7].x ,y: points[0][0].y - tempInput.wingLength * tempInput.wingGradient - tempInput.wingH1 - tempInput.wingH2},
-                    points[0][7], points[0][6], points[0][5], points[0][4], points[0][3], points[0][2], points[0][1]]
+        { x: points[0][0].x - tempInput.wingLength, y: points[0][0].y - tempInput.wingLength * tempInput.wingGradient },
+        { x: points[0][0].x - tempInput.wingLength, y: points[0][0].y - tempInput.wingLength * tempInput.wingGradient - tempInput.wingH1 },
+        { x: points[0][7].x, y: points[0][0].y - tempInput.wingLength * tempInput.wingGradient - tempInput.wingH1 - tempInput.wingH2 },
+        points[0][7], points[0][6], points[0][5], points[0][4], points[0][3], points[0][2], points[0][1]]
 
     let wingPt1 = [];
-    wingPoints.forEach(pt => wingPt1.push(ToGlobalPoint3(abutPoints[0],pt)))
+    wingPoints.forEach(pt => wingPt1.push(ToGlobalPoint3(abutPoints[0], pt)))
     let cos = abutPoints[0].normalCos
     let sin = abutPoints[0].normalSin
     let dx = tempInput.wingWallThick * cos
     let dy = tempInput.wingWallThick * sin
     // console.log(wingPoints, wingPt1)
-    let wingPt2 = [{x: wingPt1[0].x + dx, y: wingPt1[0].y + dy, z: wingPt1[0].z},
-                    {x: wingPt1[1].x + dx, y: wingPt1[1].y + dy, z: wingPt1[1].z},
-                    {x: wingPt1[2].x + dx, y: wingPt1[2].y + dy, z: wingPt1[2].z},
-                    {x: wingPt1[3].x + dx, y: wingPt1[3].y + dy, z: wingPt1[3].z},];
+    let wingPt2 = [{ x: wingPt1[0].x + dx, y: wingPt1[0].y + dy, z: wingPt1[0].z },
+    { x: wingPt1[1].x + dx, y: wingPt1[1].y + dy, z: wingPt1[1].z },
+    { x: wingPt1[2].x + dx, y: wingPt1[2].y + dy, z: wingPt1[2].z },
+    { x: wingPt1[3].x + dx, y: wingPt1[3].y + dy, z: wingPt1[3].z },];
     for (let i of [6, 5, 4, 3, 2, 1]) {
         pt1 = model["Start"]["points"][0][i];
         pt2 = model["Start"]["points"][1][i];
-        let l = Math.sqrt((pt1.x - pt2.x)**2 + (pt1.y - pt2.y)**2 + (pt1.z - pt2.z)**2)
-        let l2D = Math.sqrt((pt1.x - pt2.x)**2 + (pt1.y - pt2.y)**2)
+        let l = Math.sqrt((pt1.x - pt2.x) ** 2 + (pt1.y - pt2.y) ** 2 + (pt1.z - pt2.z) ** 2)
+        let l2D = Math.sqrt((pt1.x - pt2.x) ** 2 + (pt1.y - pt2.y) ** 2)
         wingPt2.push(DividingPoint(pt1, pt2, tempInput.wingWallThick * l / l2D));
     }
-    model["leftWing"] = { "points": [wingPt1, wingPt2], "ptGroup": [[0,7,8,9],[0,1,2,6,7],[2,3,4,5,6]]  }
+    model["leftWing"] = { "points": [wingPt1, wingPt2], "ptGroup": [[0, 7, 8, 9], [0, 1, 2, 6, 7], [2, 3, 4, 5, 6]] }
+
+    let HPt = []
+    for (let i of [4, 5, 6, 7]) {
+        dx = tempInput.wingHaunch * sin
+        dy = - tempInput.wingHaunch * cos
+        dz = 0
+        pt1 = wingPt1[i];
+        pt2 = wingPt2[i];
+        let l = Math.sqrt((pt1.x - pt2.x) ** 2 + (pt1.y - pt2.y) ** 2 + (pt1.z - pt2.z) ** 2)
+        let l2D = Math.sqrt((pt1.x - pt2.x) ** 2 + (pt1.y - pt2.y) ** 2)
+        let pt1 = {x : wingPt[i].x +dx, y : wingPt[i].y +dy, z : wingPt[i].z+dz}
+        let pt2 = DividingPoint(pt1, pt2, tempInput.wingHaunch * l / l2D);
+        HPt.push([wingPt2[i],pt1, pt2 ]);
+    }
+    model["leftWingH1"] = { "points": HPt, }
     return model
 }
