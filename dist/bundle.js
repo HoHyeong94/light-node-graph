405993d189857bde591ad8c283147eced567c5bd
jayhans <dr.lim@nexivil.com>
2020-09-04 18:21:04 +0900

    m

--- dist/bundle.js
+++ dist/bundle.js
@@ -11717,7 +11717,19 @@
           h1 : 1300
       };
 
-      let pts0 = model.girder.points[0];
+
+      let [name, h1, h2, h3, h4, h5, l1, l2, l3] = shapeData[0];
+      let pts = [
+          { x: - l2 / 2, y: - slabThickness }, { x: - l2 / 2, y: - slabThickness - h1 },
+          { x: -l1 / 2, y: - slabThickness - h1 - h2 }, { x: -l1 / 2, y: - slabThickness - h1 - h2 - h3 },
+          { x: - l3 / 2, y: - slabThickness - h1 - h2 - h3 - h4 },
+          { x: - l3 / 2, y: - slabThickness - h1 - h2 - h3 - h4 - h5 }, { x: l3 / 2, y: - slabThickness - h1 - h2 - h3 - h4 - h5 },
+          { x: l3 / 2, y: - slabThickness - h1 - h2 - h3 - h4 },
+          { x: l1 / 2, y: - slabThickness - h1 - h2 - h3 }, { x: l1 / 2, y: - slabThickness - h1 - h2 },
+          { x: l2 / 2, y: - slabThickness - h1 }, { x: l2 / 2, y: - slabThickness },
+      ];
+
+      let pts0 = pts;
       
       pts0.splice(0,0,{x : -endShape.b0/2, y : pts0[0].y });
       pts0.push({x :endShape.b0/2, y : pts0[0].y });
@@ -11725,7 +11737,7 @@
       let newPts0 = [];
       pts0.forEach(pt => newPts0.push(ToGlobalPoint(cp, pt)));
 
-      let pts1 = model.girder.points[0];
+      let pts1 = pts;
       pts1.splice(0,0,{x : -endShape.b1/2, y : pts1[0].y });
       pts1.push({x :endShape.b1/2, y : pts1[0].y });
       cp = pointDict[shapeData[0][0]];
