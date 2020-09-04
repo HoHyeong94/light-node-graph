c86c031a413e004a4a9d1a9d72580dcc2146a93f
jayhans <dr.lim@nexivil.com>
2020-09-04 16:41:29 +0900

    m

--- diSsolve/PSC/module.js
+++ diSsolve/PSC/module.js
@@ -10,35 +10,36 @@ const shapeData = [
     ["G1P1", 160, 100, 920, 120, 200, 240, 230, 330],
 ]
 
-export function GirderPointGen(pointData){
+export function GirderPointGen(pointData) {
     let result = {}
-    for (let i in pointData){
+    for (let i in pointData) {
         // let [name, benchmark, offset] = pointData[i];
-        console.log(pointData[i][0], pointData[i][2])
-        result[pointData[i][0]] = { x: 0, y: pointData[i][2], z: 2000 , normalCos: 1, normalSin: 0 }
+        if (pointData[i][0]) {
+            result[pointData[i][0]] = { x: 0, y: pointData[i][2], z: 2000, normalCos: 1, normalSin: 0 }
+        }
     }
     return result
 }
 
 export function IGirderSection(pointDict, shapeData) {
-    let model = {"girder":{"points":[], "ptGroup" : []}};
+    let model = { "girder": { "points": [], "ptGroup": [] } };
     let slabThickness = 300; //슬래브두께 + 헌치 + 포장두께 
     for (let i in shapeData) {
         let [name, h1, h2, h3, h4, h5, l1, l2, l3] = shapeData[i];
         let cp = pointDict[name];
         let pts = [
-            { x: - l2/2, y: - slabThickness }, { x:  - l2/2, y: - slabThickness - h1 },
+            { x: - l2 / 2, y: - slabThickness }, { x: - l2 / 2, y: - slabThickness - h1 },
             { x: -l1 / 2, y: - slabThickness - h1 - h2 }, { x: -l1 / 2, y: - slabThickness - h1 - h2 - h3 },
-            { x:  - l3 / 2, y: - slabThickness - h1 - h2 - h3 - h4 },
-            { x:  - l3 / 2, y: - slabThickness - h1 - h2 - h3 - h4 - h5 }, { x: l3 / 2, y: - slabThickness - h1 - h2 - h3 - h4 - h5 },
+            { x: - l3 / 2, y: - slabThickness - h1 - h2 - h3 - h4 },
+            { x: - l3 / 2, y: - slabThickness - h1 - h2 - h3 - h4 - h5 }, { x: l3 / 2, y: - slabThickness - h1 - h2 - h3 - h4 - h5 },
             { x: l3 / 2, y: - slabThickness - h1 - h2 - h3 - h4 },
-            { x: l1 / 2, y: - slabThickness - h1 - h2 - h3 },{ x: l1 / 2, y: - slabThickness - h1 - h2},
-            { x: l2 / 2, y: - slabThickness - h1  },{ x: l2 / 2, y: - slabThickness},
+            { x: l1 / 2, y: - slabThickness - h1 - h2 - h3 }, { x: l1 / 2, y: - slabThickness - h1 - h2 },
+            { x: l2 / 2, y: - slabThickness - h1 }, { x: l2 / 2, y: - slabThickness },
         ];
         let newPts = [];
-        pts.forEach(pt => newPts.push(ToGlobalPoint(cp,pt)));
+        pts.forEach(pt => newPts.push(ToGlobalPoint(cp, pt)));
         model.girder.points.push(newPts);
-        model.girder.ptGroup = [[0,1,2,9,10,11], [2,3,8,9], [3,4,5,6,7,8]]
+        model.girder.ptGroup = [[0, 1, 2, 9, 10, 11], [2, 3, 8, 9], [3, 4, 5, 6, 7, 8]]
     }
     return model
 }
