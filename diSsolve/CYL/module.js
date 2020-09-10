ec5424d0d412360ddafaf881cc54cfdf3d7d05a3
dem <dem1455@naver.com>
2020-09-10 10:46:44 +0900

    444

--- diSsolve/CYL/module.js
+++ diSsolve/CYL/module.js
@@ -9,7 +9,7 @@ export function CylinderModelView(top,buttom,height) {
     //var cylinder = new THREE.Mesh( geometry, material );
     geometry.computeFaceNormals();
     let mesh = new THREE.Mesh(geometry, meshMaterial)
-    mesh.setPosition(500,500,500)
+    mesh.Position.set(500,500,500)
     group.add(new THREE.Mesh(geometry, meshMaterial));
 
     return group
