59da41924c39d00a068eff2e998ad17fd7a51fa8
dem <dem1455@naver.com>
2020-09-10 10:55:51 +0900

    666

--- diSsolve/CYL/module.js
+++ diSsolve/CYL/module.js
@@ -5,11 +5,11 @@ import { THREE, SpriteText } from "global";
 export function CylinderModelView(top,buttom,height) {
     const group = new THREE.Group();
     const geometry = new THREE.CylinderGeometry(top,buttom,height);
-    const meshMaterial = new THREE.MeshNormalMaterial()
+    const meshMaterial = new THREE.MeshStandardMaterial()
     //var cylinder = new THREE.Mesh( geometry, material );
     geometry.computeFaceNormals();
     const mesh = new THREE.Mesh(geometry, meshMaterial)
-    mesh.Position.set(200,200,0)
+    //mesh.Position.set(200,200,0)
     group.add(new THREE.Mesh(geometry, meshMaterial));
 
     return group
