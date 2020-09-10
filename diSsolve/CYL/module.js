8b1ff3170277cd281e602d16915aaac5db0f5136
dem <dem1455@naver.com>
2020-09-10 11:32:42 +0900

    tt

--- diSsolve/CYL/module.js
+++ diSsolve/CYL/module.js
@@ -7,16 +7,16 @@ export function CylinderModelView(top,buttom,height) {
     const geometry = new THREE.CylinderGeometry(top,buttom,height);
     const meshMaterial = new THREE.MeshNormalMaterial()
    
-    //geometry.computeFaceNormals();
+    geometry.computeFaceNormals();
     const mesh = new THREE.Mesh(geometry, meshMaterial)
-    //mesh.position.set(-1000,1000,1000)
+    mesh.position.set(-100,100,100)
     console.log("메쉬위치")
     console.log(mesh)
     console.log(mesh.position)
-    //mesh.Position.set(200,200,0)
+    
 
     group.add(new THREE.Mesh(geometry, meshMaterial));
-    group.position.set(-1000,1000,1000)
+    
     console.log("그룹위치")
     console.log(group)
     console.log(group.position)
