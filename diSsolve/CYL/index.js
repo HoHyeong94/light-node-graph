0be4dd3e7caf2885b869efe53f5b1edf0e1eeedb
dem <dem1455@naver.com>
2020-09-10 10:31:26 +0900

    111

--- diSsolve/CYL/index.js
+++ diSsolve/CYL/index.js
@@ -2,9 +2,9 @@ import { sceneAdder, THREE } from "global";
 import {CylinderModelView } from "./module"
 
 export function Cylinderview() {
-    this.addInput("length", "arr");
-    //this.addInput("bottom","number")
-    //this.addInput("height", "number");
+    this.addInput("top", "number");
+    this.addInput("bottom","number")
+    this.addInput("height", "number");
     
   }
 
@@ -13,10 +13,9 @@ export function Cylinderview() {
 Cylinderview.prototype.onExecute = function () {
     
     
-    let tmpMesh = CylinderModelView(this.getInputData(0))// this.getInputData(1),this.getInputData(2))
+    let tmpMesh = CylinderModelView(this.getInputData(0),this.getInputData(1),this.getInputData(2))
     sceneAdder({name:'cy', layer:0, mesh:tmpMesh, meta:{part:this.getInputData(2)}});
     
     //console.log(tmpMesh)
     //console.log(getInputData(2))
   }
-  
--- diSsolve/CYL/module.js
+++ diSsolve/CYL/module.js
@@ -2,9 +2,9 @@ import { THREE, SpriteText } from "global";
 
 
 
-export function CylinderModelView(length) {
+export function CylinderModelView(top,buttom,height) {
     let group = new THREE.Group();
-    let geometry = new THREE.CylinderGeometry( length);
+    let geometry = new THREE.CylinderGeometry(top,buttom,height);
     let meshMaterial = new THREE.MeshNormalMaterial()
     //var cylinder = new THREE.Mesh( geometry, material );
     geometry.computeFaceNormals();
