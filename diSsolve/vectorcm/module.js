4cd107c7a1447282c7fcde5a0a7412ff25f5c8de
dem <dem1455@naver.com>
2020-09-10 16:16:07 +0900

    111

--- diSsolve/vectorcm/module.js
+++ diSsolve/vectorcm/module.js
@@ -5,13 +5,13 @@ export function VectorModelView(point1,point2,point3) {
     const group = new THREE.Group();
     const geometry = new THREE.Geometry();
     const meshMaterial = new THREE.MeshNormalMaterial()
+    meshMaterial.side = THREE.DoubleSide
     //meshMaterial.wireFrame = true
 
     geometry.vertices.push(new THREE.Vector3(point1[0][0],point1[0][1],point1[0][2]));
     geometry.vertices.push(new THREE.Vector3(point2[0][0],point2[0][1],point2[0][2]));
     geometry.vertices.push(new THREE.Vector3(point3[0][0],point3[0][1],point3[0][2]));
-    geometry.computeFaceNormals();
-
+    
     const face = new THREE.Face3(0, 1, 2)
     geometry.faces.push(face)
     geometry.computeFaceNormals();
@@ -28,7 +28,6 @@ export function VectorModelView(point1,point2,point3) {
     console.log(mesh)
     console.log(mesh.position)
     
-geometry.computeFaceNormals();
     group.add(mesh);
     //group.position.set(0,250,500)
     console.log("그룹위치")
